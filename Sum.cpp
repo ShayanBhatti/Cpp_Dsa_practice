a0c404cbb6c2a2a79ada2ea67499e6d4e3ee8d24
Muhammad Shayan <125445144+ShayanBhatti@users.noreply.github.com>
2024-04-09 23:31:24 +0500

    The Concept of Array .
    In this code array is being used to make sum and avg of number

---
+++ Sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a,b,sum;
+cout<<"Enter first value && second value "<<endl;
+cin>>a>>b;
+sum=a+b;
+cout<<"THe sum of number is :"<<sum<<endl;
+return 0;
+
+}
