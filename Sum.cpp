9fceba1470ff29d43f689e7a08634cb801639f59
Muhammad Shayan <125445144+ShayanBhatti@users.noreply.github.com>
2024-04-10 12:08:58 +0500

    Update Sum.cpp

--- Sum.cpp
+++ Sum.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
-int a,b,sum;
+int a,b;
 cout<<"Enter first value && second value "<<endl;
 cin>>a>>b;
-sum=a+b;
-cout<<"THe sum of number is :"<<sum<<endl;
+cout<<"THe sum of number is :"<<(a+b)<<endl;
 return 0;
 
 }
