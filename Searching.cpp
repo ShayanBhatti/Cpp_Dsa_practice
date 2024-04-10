7c59ca20badfc8b6672e8fb7230939c78beedce5
Muhammad Shayan <125445144+ShayanBhatti@users.noreply.github.com>
2024-04-10 12:06:23 +0500

    Simple Search

---
+++ Searching.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+   int arr[] = {1,3,5,7,8};
+   int num = 5; 			//item or search value
+   int n = 5;
+   int i = 0; 
+   int j = 0;
+   
+   cout<<"The Array Elements Are : "<<endl;	//travering
+   for(i = 0; i<n; i++) 
+   {
+      cout<<"The Array Index is = \t"<<i<<"\t \t The Array Value is : \t"<<arr[i]<<endl;
+   }
+   while(j < n)						//search method
+   {
+      if(arr[j] == num) 
+	  {
+         break;
+      }	
+      j = j + 1;
+   }
+   cout<<"Found Element : \t"<<num<<"\t \t At Postion : \t"<<j+1<<endl;
+   return 0;
+}
